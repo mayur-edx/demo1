1308e2cacf53a039f49f05a91d179b001fb610af
Mayur Parmar <m.parmar@edexa.team>
2022-06-10 18:51:50 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" onContextMenu={(e) => {
+      e.preventDefault()
+      console.log("Main mouse left click");
+    }}  onClick={(e) => {
+      console.log("dsds");
+      console.log(e.type);
+      
+      if(e.type === "contextmenu"){
+      console.log("Mouse Rigth click");
+      } else if (e.type === "clicl"){
+        console.log("mouse left click");
+      }
+    }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hello</h1>        
       </header>
     </div>
   );
